0ce14094afc8b31332dea3fd17c945fb7afe300c
Marin Leontenko <marin.leontenko@gmail.com>
2018-02-16 21:29:59 +0100

    search function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAmBDc2VkEJWle2VCHcuxeA3q7F-oIUO54';
 
 // Create new component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-        );
+class App extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+            );
+    }
 }
 
 // Take component's generated HTML and put it in the DOM
