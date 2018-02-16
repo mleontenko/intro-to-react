c732d6362616c98c847613b8588566aa36e6d516
Marin Leontenko <marin.leontenko@gmail.com>
2018-02-16 13:04:42 +0100

    search bar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAmBDc2VkEJWle2VCHcuxeA3q7F-oIUO54';
 
 // Create new component
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+        );
 }
 
 // Take component's generated HTML and put it in the DOM
