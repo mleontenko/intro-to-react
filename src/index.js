927d03c174f29a24f84914225d502ef16d9c03ac
Marin Leontenko <marin.leontenko@gmail.com>
2018-02-16 11:53:32 +0100

    youtube api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAmBDc2VkEJWle2VCHcuxeA3q7F-oIUO54';
+
 // Create new component
 const App = () => {
     return <div>Hi!</div>;
